493f353be01ab63c976244038fc4f7b8d77b7382
Myoko <92731952+Myoko-1110@users.noreply.github.com>
2022-04-12 19:26:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const { Client, Intents } = require('discord.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
-const msg = require('./msg.js');
-client.msg = msg;
 
 client.on ('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
