10ede250a32bf69dcb165f3741531ed2fb5c0e69
Myoko <92731952+Myoko-1110@users.noreply.github.com>
2022-04-13 22:03:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
-const { Client, Intents } = require('discord.js')
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on ('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 });
 
-let Switch = 0; //関数"Switch"に0を設定
+const prefix = "a!"; //prefixを設定
+let Switch = "on"; //関数"Switch"に"on"を設定
+
 
 client.on ('messageCreate', (message) => {
-    if (message.content === '!ping') {
+    if (message.content === `${prefix}ping`) {
         message.reply('pong!'); //メッセージが"!ping"だったら返信
     }
 
+    if (message.content === `${prefix}off`) {
+        if (Switch === "off") {
+            message.reply('メッセージ返信機能はすでにオフです。'); //関数"Switch"が"off"だったら"すでにオフです。"を返信
+        }
+        else {
+            Switch = "off";
+            message.reply('メッセージ返信機能をオフにしました。'); //該当しない場合は関数"Switch"を"off"に設定
+        }
+    }
+
+    if (message.content === `${prefix}on`) {
+        if (Switch === "on") {
+            message.reply('メッセージ返信機能はすでにオンです。'); //関数"Switch"が"on"だったら"すでにオンです。"を返信
+        }
+        else {
+            Switch = "on";
+            message.reply('メッセージ返信機能をオンにしました。'); //該当しない場合は関数"Switch"を"on"に設定
+        }
+    }
 
     if (message.author.bot) return;
-    if (Switch === 1) return; //関数"Switch"が1だったら返信をしない
-    if (message.content === 'a!on') return; //メッセージが"a!on"だったら返信をしない
-    if (message.content === 'a!off') return; //メッセージが"a!off"だったら返信をしない
-    if (message.content === '!ping') return; //メッセージが"!ping"だったら返信をしない
+    if (Switch === "off") return; //関数"Switch"が1だったら返信をしない
+    if (message.content === `${prefix}on`) return;
+    if (message.content === `${prefix}off`) return;
+    if (message.content === `${prefix}ping`) return;
     let arr = [
         //ここからランダムで送るメッセージ
 
@@ -70,7 +91,7 @@ client.on ('messageCreate', (message) => {
         {files: ['voice/確証がほしいの確証が.mp3']}
 
         //ここで終わり
-    ]
+    ];
     let random = Math.floor(Math.random() * arr.length);
     let result = arr[random];
     message.channel.send(result);
@@ -79,29 +100,11 @@ client.on ('messageCreate', (message) => {
 //　↑こんな感じで "文字1", "文字2" ってやってください
 //　文字は"でくくって、文字と文字の間はカンマとスペースを忘れずに付けてください。
 
-client.on ('messageCreate', (message) => {
-    if (message.content === 'a!on') {
-        if (Switch === 0) {
-            message.reply('すでにオンです。'); //関数"Switch"が0だったら"すでにオンです。"を返信
-        }
-        else {
-            Switch = 0;
-            message.reply('メッセージ返信をオンにしました。'); //該当しない場合は関数"Switch"を"0"に設定
-        }
-    }
-});
 
-client.on ('messageCreate', (message) => {
-    if (message.content === 'a!off') {
-        if (Switch === 1) {
-            message.reply('すでにオフです。'); //関数"Switch"が"1"だったら"すでにオンです。"を返信
-        }
-        else {
-            Switch = 1;
-            message.reply('メッセージ返信をオフにしました。'); //該当しない場合は関数"Switch"を"1"に設定
-        }
-    }
-});
+
+
 
 
 client.login("OTU4MzExOTQ3Mzc0MzA5Mzc2.YkLfjA.hxnE4DRvIOLLaQReojj9LbwlQcc");
+
+//client.login("OTYwODgwNTk4MzY3NDczNzQ3.Ykw3yg.sA8wyrHYtncwSq6E-cBa3Ylw4Os");
